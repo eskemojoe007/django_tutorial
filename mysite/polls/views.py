6eb50b8470510bec450b35a745b48657a4253943
eskemojoe007 <David.Folkner@gmail.com>
2018-04-09 19:34:07 -0400

    Through Step 3
    Re-did on my own as much as possible.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("Welcome...ya filthy animal")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request,'polls/index.html',context={'latest_question_list':latest_question_list})
+
+def detail(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/detail.html',context={'question':question})
+    # return HttpResponse("Looking at detail for {}".format(question_id))
+
+def results(request,question_id):
+    return HttpResponse("Looking at results for {}".format(question_id))
+
+def vote(request,question_id):
+    return HttpResponse("Looking at vote for {}".format(question_id))
