5c3dd9128f72ff9d223b813aa7de890cd38918e8
Folkner, David Erik <david.folkner@ge.com>
2018-04-10 09:14:16 -0400

    Last bit of merging

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,12 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path,include
-=======
 from django.urls import path, include
 
->>>>>>> 5f74bac06b31a454d097886aa93fbceb0408bb33
 
 urlpatterns = [
     path('polls/',include('polls.urls')),
